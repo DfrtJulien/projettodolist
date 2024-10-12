2166bb06118a64b065bca97408d214b38f774f91
DfrtJulien <de.freitas.julien78@gmail.com>
2024-10-12 11:35:45 +0200

    fin de la vu enfant pour voir ses taches

--- src/Views/task/myTasks.view.php
+++ src/Views/task/myTasks.view.php
@@ -1,8 +1,23 @@
 <?php
-    require_once(__DIR__ . '/../partials/head.php');
+require_once(__DIR__ . '/../partials/head.php');
 ?>
 <h1>Mes taches : </h1>
-
 <?php
-    require_once(__DIR__ . '/../partials/footer.php');
+if ($resultTask) {
+    foreach ($resultTask as $myTask) {
+
+
+?>
+        <div class="bgg">
+            <p>Déscription : <?= $myTask->getContent() ?> </p>
+            <p>Date de création <?= $myTask->getCreationDate() ?></p>
+            <p>Du : <?= $myTask->getStartTask()  ?> au: <?= $myTask->getStopTask() ?></p>
+            <p>Points : <?= $myTask->getPoint() ?></p>
+        </div>
+<?php
+    }
+}
+?>
+<?php
+require_once(__DIR__ . '/../partials/footer.php');
 ?>
